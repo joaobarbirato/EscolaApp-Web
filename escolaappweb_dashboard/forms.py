36f5ec7db8b91a1a41c5ca4ea99f0e654b8fa263
joao barbirato <joaobarbirato@gmail.com>
2018-12-06 22:43:07 -0200

    Adicionados comentários; meta-atributos em base.html

--- escolaappweb_dashboard/forms.py
+++ escolaappweb_dashboard/forms.py
@@ -2,35 +2,36 @@ from django import forms
 from escolaappweb_dashboard.models import Turma, Pai, Aluno, Materia
 from escolaappweb_dashboard.constants import REGEX_TELEFONE
 
-# CRUD Alunos, Pais, Turmas, Matérias
+# Formulários de Criação de Alunos, Pais, Turmas, Matérias
 
+"""
+    TurmaForm
+    Formulário de criação do modelo Turma
+"""
 class TurmaForm(forms.ModelForm):
     class Meta:
         model = Turma
         fields = '__all__'
 
-
+"""
+    MateriaForm
+    Formulário de criação do modelo Matéria
+"""
 class MateriaForm(forms.ModelForm):
     class Meta:
         model = Materia
         fields = '__all__'
 
+
 from re import compile as verifica_com_regex
+
+"""
+    PaiForm
+    Formulário de criação do modelo Pai
+    @metodos:
+        - clean: validação de campos
+"""
 class PaiForm(forms.ModelForm):
-    # TODO: telefone Precisa ser validado com regex.
-    # portanto, não é possivel utilizar ModelForm (?)
-    # nome = forms.CharField(label='Insira o nome')
-    # telefone = forms.RegexField(REGEX_TELEFONE
-    #     # 99 99999 9999
-    #     # 99 9999 9999
-    #     # 99 99999-9999
-    #     # 99 9999-9999
-    #     # (99) 99999 9999
-    #     # (99) 9999 9999
-    #     # (99) 99999-9999
-    #     # (99) 9999-9999
-    # )
-    
     class Meta:
         model = Pai
         fields = [
@@ -38,11 +39,14 @@ class PaiForm(forms.ModelForm):
             'telefone',
         ]
 
+    # validação de campos
     def clean(self, *args, **kw):
         nome = self.cleaned_data.get('nome')
         telefone = self.cleaned_data.get('telefone')
         if any(char.isdigit() for char in nome):
             raise forms.ValidationError('Caractere inválido.')
+
+        # Verifica se o telefone está no padrão estabelecido na máscara
         pattern = verifica_com_regex(REGEX_TELEFONE)
         print(pattern.match(telefone))
         if pattern.match(telefone) is None:
@@ -51,6 +55,10 @@ class PaiForm(forms.ModelForm):
         return super(PaiForm, self).clean(*args,**kw)
 
 
+"""
+    AlunoForm
+    Formulário de criação do modelo Aluno
+"""
 class AlunoForm(forms.ModelForm):
     class Meta:
         model = Aluno
--- escolaappweb_dashboard/models.py
+++ escolaappweb_dashboard/models.py
@@ -2,9 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-def get_all(model=None):
-    return model.objects.all()
-
+"""
+    @modelo: Turma
+    @atr:
+        - nome: Nome da turma (ex. turma1)
+        - ano: Ano da turma (ex. 2o ano)
+"""
 class Turma(models.Model):
     nome = models.CharField(max_length=100, blank=False)
     ano = models.CharField(max_length=100, blank=False)
@@ -21,6 +24,11 @@ class Turma(models.Model):
         return self.nome
 
 
+"""
+    @modelo: Materia
+    @atr:
+        - nome: Nome da matéria (ex. matemática)
+"""
 class Materia(models.Model):
     nome = models.CharField(max_length=100, blank=False)
 
@@ -36,6 +44,13 @@ class Materia(models.Model):
         return self.nome
 
 
+"""
+    @modelo: Pai
+    @atr:
+        - nome: Nome do pai (ex. José)
+        - telefone: Número de telefone - posteriormente será utilizado no aplicativo
+            (ex. 99 99999 9999)
+"""
 class Pai(models.Model):
     nome = models.CharField(max_length=100, blank=False)
     telefone = models.CharField(max_length=20, blank=False, default='00000000000000000000')
@@ -52,6 +67,15 @@ class Pai(models.Model):
         return self.nome
 
 
+"""
+    @modelo: Aluno
+    @atr:
+        - nome: Nome do(a) aluno(a) (ex. Joãozinho)
+        - turma (Chave Estrangeira p/ modelo Turma):
+            turma a qual o(a) aluno(a) pertence
+        - pai (Chave Estrangeira p/ modelo Pai):
+            pai do(a) aluno(a)
+"""
 class Aluno(models.Model):
     nome = models.CharField(max_length=100, blank=False)
     turma = models.ForeignKey(Turma, on_delete=models.SET_NULL, null=True, blank=True)
@@ -68,7 +92,11 @@ class Aluno(models.Model):
     def __str__(self):
         return self.nome
 
-
+""" 
+    get_all_by_key
+    Retorna todos os objetos de um determinado modelo
+    @key: parametro identificador de modelos
+"""
 def get_all_by_key(key=None):
     if key == 'turma':
         return Turma.objects.all()
@@ -79,6 +107,21 @@ def get_all_by_key(key=None):
     elif key == 'aluno':
         return Aluno.objects.all()
 
+# Funções auxiliares
+
+""" 
+    get_all
+    Retorna todos os objetos de um determinado modelo 
+    @model: modelo que os objetos são.
+"""
+def get_all(model=None):
+    return model.objects.all()
+
+""" 
+    get_model_by_key
+    Retorna uma referência ao modelo especificado
+    @key: parametro identificador de modelos
+"""
 def get_model_by_key(key=None):
     if key == 'turma':
         return Turma
